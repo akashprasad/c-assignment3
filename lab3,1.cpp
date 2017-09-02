4779f7442cc1bad44b28431c687cb2a559c0f41d
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-02 13:11:53 +0530

    Create lab3,1.cpp

---
+++ lab3,1.cpp
@@ -0,0 +1,16 @@
+ # c-assignment3
+ // question 1
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "Enter the two numbers : ";
+	cin >> a >> b;
+	if (a > b)
+		cout << a << " is maximum";
+	else
+		cout << b << " is maximum";
+  return 0;
+}
+
