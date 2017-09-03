a14b35ea1fb5c6c92030beb9a26ea5a290ba5ecc
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-03 14:13:40 +0530

    Create lab3,3.cpp

---
+++ lab3,3.cpp
@@ -0,0 +1,18 @@
+# c-assignment3
+// question 3
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number : ";
+    cin>>a;
+    if(a>0)
+    cout<<"The number is positive";
+    else if(a<0)
+    cout<<"The number is negative";
+    else
+    cout<<"The number is zero";
+    return 0;
+}
