99c19981cf398ca1cb3d4a00aa5976412afe2d4a
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-03 15:08:16 +0530

    Create lab3,10.cpp

---
+++ lab3,10.cpp
@@ -0,0 +1,15 @@
+# c-assignment3
+//question 10
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cout<<"Enter a alphabet : ";
+    cin>>a;
+    if(a>=65 && a<=90)
+    cout<<"The alphabet is uppercase.";
+    else
+    cout<<"The alphabet is lowercase.";
+    return 0;
+}
