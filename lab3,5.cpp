8825de8e494e65efbf6aa7330896328622e5c5aa
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-03 14:23:14 +0530

    Create lab3,5.cpp

---
+++ lab3,5.cpp
@@ -0,0 +1,15 @@
+# c-assignment3
+// question 5
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a number : ";
+    cin>>a;
+    if(a%2==0)
+    cout<<"The number is even";
+    else
+    cout<<"The number is odd";
+    return 0;
+}
