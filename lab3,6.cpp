4c71544f7d68a6c90300bb6b7dd34f998967e382
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-03 14:27:17 +0530

    Create lab3,6.cpp

---
+++ lab3,6.cpp
@@ -0,0 +1,15 @@
+# c-assignment3
+// question 6
+#include <iostream>
+using namespace std;
+int main()
+{
+    int y;
+    cout<<"Enter the year : ";
+    cin>>y;
+    if(y%4==0)
+    cout<<"The year is leap year.";
+    else
+    cout<<"The year is not leap year.";
+    return 0;
+}
