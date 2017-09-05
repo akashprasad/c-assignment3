491527cb4de11620abc70ac5c24bc2729ab4a4ff
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-05 16:29:16 +0530

    Create lab3,24.cpp

---
+++ lab3,24.cpp
@@ -0,0 +1,15 @@
+# c-assignment3
+// question 24
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  a=97;
+  while(a<=122)
+  {
+    cout<<(char)a<<"	";
+    a++;
+  }
+  return 0;
+}
