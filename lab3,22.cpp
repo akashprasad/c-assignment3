c799d561824a249b31e7be231a1a8400a7ed2697
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-05 16:25:41 +0530

    Create lab3,22.cpp

---
+++ lab3,22.cpp
@@ -0,0 +1,18 @@
+# c-assignment3
+// question 22
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,x;
+  x=1;
+  cout<<"Enter the nth tearm : ";
+  cin>>n;
+ while(x<=n)
+ {
+    cout<<x;
+    cout<<endl;
+    x++;
+  }
+  return 0;
+}
