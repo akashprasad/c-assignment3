994eef39bcd22c96c1a83c89bf0f4ee163bdcec6
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-05 16:26:28 +0530

    Create lab3,23.cpp

---
+++ lab3,23.cpp
@@ -0,0 +1,16 @@
+# c-assignment3
+// question 23
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,x;
+  cout<<"Enter the nth term : ";
+  cin>>n;
+  while(n!=0)
+  {
+    cout<<n<<endl;
+    n--;
+  }
+  return 0;
+}
