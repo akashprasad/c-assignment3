1e9568bdd987dc976a70f1b1343ef01c2bd4e075
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-05 16:38:56 +0530

    Create lab3,25.cpp

---
+++ lab3,25.cpp
@@ -0,0 +1,16 @@
+# c-assignment3
+//question 25
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x;
+  x=1;
+  while(x<=100)
+  {
+    if(x%2==0)
+      cout<<x<<"		";
+    x++;
+  }
+  return 0;
+}
