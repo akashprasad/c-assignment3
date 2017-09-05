d7cccc1c05080a007bf8cb8bf6c8ae4ddc51ac34
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-05 17:02:53 +0530

    Create lab3,30.cpp

---
+++ lab3,30.cpp
@@ -0,0 +1,16 @@
+# c-assignment3
+//question 30
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n,m;
+  cout<<"Enter any number : ";
+  cin>>n;
+  for(int x=1;x<=10;x++)
+  { 
+    cout<<n<<" * "<<x<<" =  "<<(n*x);
+    cout<<endl;
+  }
+  return 0;
+}
