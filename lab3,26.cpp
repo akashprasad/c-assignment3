37ddb2143dbd61ec6194ae355f0de3f2f266a3fc
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-05 16:39:53 +0530

    Create lab3,26.cpp

---
+++ lab3,26.cpp
@@ -0,0 +1,16 @@
+# c-assignment3
+// question 26
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  a=1;
+  while(a<=100)
+  {
+    if(a%2!=0)
+      cout<<a<<"	";
+    a++;
+  }
+  return 0;
+}
