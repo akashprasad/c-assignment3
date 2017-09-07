59fe591bd4d6bb07daaf19434db3ff3add02e7c7
akashprasad <31240956+akashprasad@users.noreply.github.com>
2017-09-07 18:51:52 +0530

    Create lab3,41.cpp

---
+++ lab3,41.cpp
@@ -0,0 +1,14 @@
+# c-assignment3
+//question 41
+#include<iostream>
+using namespace std;
+int main()
+{
+ int x;
+ cout<<"value"<<"       "<<"ASCII"<<endl;
+ for(x=0;x<=255;x++)
+ {
+   cout<<x<<"       "<<char(x)<<endl;
+ }
+ return 0;
+}
